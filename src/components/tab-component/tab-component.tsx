f648b29eecdfe1eaf51eec4d81e000ca4f0fe62d
TrymVei <tve@variant.no>
2024-04-17 19:18:57 +0200

    feat(pages): possible to render children elements in tab component

--- src/components/tab-component/tab-component.tsx
+++ src/components/tab-component/tab-component.tsx
@@ -1,17 +1,19 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, h, Element } from '@stencil/core';
 
 @Component({
   tag: 'tab-component',
   styleUrl: 'tab-component.scss',
+  shadow: true,
 })
 export class TabComponent {
-  private getText() { }
+  @Element() host: HTMLDivElement;
 
   render() {
     return (
       <div>
-        <button>Om oss</button>
+        {Array.from(this.host.children).map(child => (
+          <div innerHTML={child.outerHTML} />
+        ))}
       </div>
     );
   }
