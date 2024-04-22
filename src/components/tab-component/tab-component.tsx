e5d1945c7bdd653d2fc45dffe3b84e5e9e1d14a0
Trym Veiby <110395932+TrymVei@users.noreply.github.com>
2024-04-22 21:14:12 +0200

    feat: tabpanel-component (#3)
    * feat(pages): possible to render children elements in tab component
    
    * feat(tabID): added tab id and add them to component state
    
    * feat(tab anchor): render anchor tag for each id added to state
    
    * feat(tab title): added tab title
    
    * feat(Tab type): added type for tab object
    
    * feat(display tab): simple function to hide tabs
    
    * feat: changed from interface to type for tab
    
    * feat: possible to switch tabs and removed tab index on tabs
    
    * feat: tab navigation keyboard shortcuts
    
    * feat: focus on tabpanel on keydown space
    
    * feat: added styling for tab-component

--- src/components/tab-component/tab-component.tsx
+++ src/components/tab-component/tab-component.tsx
@@ -1,17 +1,146 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, h, Element, State, Listen } from '@stencil/core';
+import { Tab } from '../../utils/types/tab';
 
 @Component({
   tag: 'tab-component',
   styleUrl: 'tab-component.scss',
+  shadow: true,
 })
 export class TabComponent {
-  private getText() { }
+  @Element() host: HTMLDivElement;
+
+  @State() tabs: Tab[] = [];
+  @State() activeTab: number = 0;
+
+  /*
+   * Get the innerHTML of the first child of a tab
+   * @param childId: string - The id of the tab
+   * @returns string | null - The innerHTML of the first child of the tab or null if the tab does not exist
+   */
+  private getTabTitle(childId: string): string | null {
+    const childElement = document.getElementById(childId);
+    return childElement ? childElement.children.item(0).innerHTML : null;
+  }
+
+  /*
+   * Conver a tab to an object with the name and id of the tab
+   * @param name: string - The name of the tab. Usually the innerHTML of the first child of the tab.
+   * @param id: string - The id of the tab
+   * @returns Tab - An object with the name and id of the tab
+   */
+  private convertToTabObject(name: string, id: string): Tab {
+    return {
+      name,
+      id,
+    };
+  }
+
+  private setActiveTab(index: number) {
+    this.activeTab = index;
+  }
+
+  private handleTabClick(e: Event, index: number) {
+    e.preventDefault();
+    this.setActiveTab(index);
+  }
+
+  private setTabIndexes(index: number) {
+    return index === this.activeTab ? '0' : '-1';
+  }
+
+  componentWillLoad() {
+    Array.from(this.host.children).map(child => {
+      if (child instanceof HTMLElement && child.id) {
+        const id = child.id;
+        const title = this.getTabTitle(id);
+        const tabObject = this.convertToTabObject(title, id);
+        this.tabs.push(tabObject);
+      }
+    });
+    const tabPanel = this.host.shadowRoot.querySelector(`#page${1}`) as HTMLElement;
+    if (tabPanel) {
+      tabPanel.focus();
+    }
+  }
+
+  // Functions for handling arrow key navigation
+  @Listen('keydown')
+  handleGlobalKeydown(event: KeyboardEvent) {
+    if (event.key === 'ArrowLeft') {
+      this.handleArrowKey(this.activeTab, -1); // Move to the previous tab
+    } else if (event.key === 'ArrowRight') {
+      this.handleArrowKey(this.activeTab, 1); // Move to the next tab
+    } else if (event.key === 'H') {
+      this.setActiveTab(0); // Move to the first tab
+      this.focusOnTab(0);
+    } else if (event.key === 'E') {
+      this.setActiveTab(this.tabs.length - 1); // Move to the last tab
+      this.focusOnTab(this.tabs.length - 1);
+    } else if (event.key === 'Space' || event.key === ' ') {
+      this.focusOnTabPanel(this.activeTab);
+    }
+  }
+
+  private focusOnTabPanel(index: number) {
+    const tabPanel = this.host.shadowRoot.querySelector(`#tabpanel${index}`) as HTMLElement; // index + 1 because the tab index is 0-based
+    if (tabPanel) {
+      tabPanel.focus();
+    }
+  }
+
+  private focusOnTab(index: number) {
+    const tab = this.host.shadowRoot.querySelector(`#anchor${index}`) as HTMLElement;
+    if (tab) {
+      tab.focus();
+    }
+  }
+
+  private handleArrowKey(currentIndex: number, direction: number) {
+    const numTabs = this.tabs.length;
+
+    // Calculate the new index
+    let newIndex = currentIndex + direction;
+
+    // Wrap around if the index exceeds the number of tabs
+    if (newIndex < 0) {
+      newIndex = numTabs - 1;
+    } else if (newIndex >= numTabs) {
+      newIndex = 0;
+    }
+
+    // Set the active tab
+    this.setActiveTab(newIndex);
+    // Focus on the new tab
+    this.focusOnTab(newIndex);
+  }
 
   render() {
     return (
       <div>
-        <button>Om oss</button>
+        <div class="tabs">
+          <ul class="tabs__list" role="tablist">
+            {this.tabs.map((tab: Tab, index: number) => {
+              return (
+                <li class="tabs__list_item" role="presentation">
+                  <a
+                    class="tabs__link"
+                    role="tab"
+                    id={`anchor${index}`}
+                    tabindex={this.setTabIndexes(index)}
+                    aria-controls={tab.id}
+                    href={`#${tab.id}`}
+                    onClick={e => this.handleTabClick(e, index)}
+                  >
+                    {tab.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {Array.from(this.host.children).map((child: HTMLElement, index: number) => (
+          <div class="tabpanel" tabindex="0" id={`tabpanel${index}`} role="tabpanel" style={{ display: this.activeTab == index ? 'block' : 'none' }} innerHTML={child.outerHTML} />
+        ))}
       </div>
     );
   }
